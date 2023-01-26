ff544cb9b06a14f0da892030e4858d3a03551fb7
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-01-26 15:49:08 -0500

    cambio de mobile

--- app/app.mjs
+++ app/app.mjs
@@ -1,7 +1,7 @@
 export const handler = async(event) => {
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from Lambda! v0.0.1'),
+        body: JSON.stringify('Hello from Lambda! v0.0.2'),
     };
     return response;
 };
