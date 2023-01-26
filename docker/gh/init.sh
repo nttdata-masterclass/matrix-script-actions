e899f89cbf7d9350b75dc834c5e3e1e0b32fed3c
Ivan Echegaray Avendaño <ivan.echegaray@outlook.com>
2023-01-26 15:00:55 -0500

    agregando release

---
+++ docker/gh/init.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+echo "GITHUB_TOKEN: ${GITHUB_TOKEN}"
+
+# github login
+echo ${GITHUB_TOKEN} | gh auth login --with-token
+
+# clonando repositorio
+gh repo clone ${GITHUB_REPOSITORY}
+
+cd $(echo ${GITHUB_REPOSITORY} | cut -d "/" -f2)/
+
+GITHUB_TAGS=$(gh release list)
+
+if [ "$(gh release list | grep Latest | awk '{print $9 " " $2}')" == " Latest" ]
+then
+  MAJOR_VERSION=$(gh release list | grep Latest | awk '{print $9 " " $1}' | cut -d "v" -f2 | cut -d "." -f1)
+  MINOR_VERSION=$(gh release list | grep Latest | awk '{print $9 " " $1}' | cut -d "v" -f2 | cut -d "." -f2)
+  PATCH_VERSION=$(gh release list | grep Latest | awk '{print $9 " " $1}' | cut -d "v" -f2 | cut -d "." -f3)
+  PATCH_VERSION=$((PATCH_VERSION+1))
+  gh release create v${MAJOR_VERSION}.${MINOR_VERSION}.${PATCH_VERSION} --generate-notes
+else
+  gh release create v0.0.1 --generate-notes
+fi
